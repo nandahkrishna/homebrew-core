65a1bc8a8cc2ad9c7f1e85d642e07b27bab3f006
Nanda H Krishna <me@nandahkrishna.com>
2021-09-20 15:41:31 -0400

    libsoup 3.0.0

--- Formula/libsoup.rb
+++ Formula/libsoup.rb
@@ -1,10 +1,9 @@
 class Libsoup < Formula
   desc "HTTP client/server library for GNOME"
   homepage "https://wiki.gnome.org/Projects/libsoup"
-  url "https://download.gnome.org/sources/libsoup/2.74/libsoup-2.74.0.tar.xz"
-  sha256 "33b1d4e0d639456c675c227877e94a8078d731233e2d57689c11abcef7d3c48e"
+  url "https://download.gnome.org/sources/libsoup/3.0/libsoup-3.0.0.tar.xz"
+  sha256 "cde05f3e20114ce985cbd03d575ff74e5e41e70a218e5eb3068c4bf4710dbf2b"
   license "LGPL-2.0-or-later"
-  revision 1
 
   bottle do
     sha256 arm64_big_sur: "18fb8d1e8cec64c10c52a3ded030d3fe4654b731f8b02b20dd8ffec3341d2f6e"
@@ -40,10 +39,16 @@ class Libsoup < Formula
       #include <libsoup/soup.h>
 
       int main(int argc, char *argv[]) {
-        SoupMessage *msg = soup_message_new("GET", "https://brew.sh");
+        SoupMessage *msg = soup_message_new(SOUP_METHOD_GET, "https://brew.sh");
         SoupSession *session = soup_session_new();
-        soup_session_send_message(session, msg); // blocks
-        g_assert_true(SOUP_STATUS_IS_SUCCESSFUL(msg->status_code));
+        GError *error = NULL;
+        GBytes *bytes = soup_session_send_and_read(session, msg, NULL, &error); // blocks
+
+        if(error) {
+          g_error_free(error);
+          return 1;
+        }
+
         g_object_unref(msg);
         g_object_unref(session);
         return 0;
@@ -56,7 +61,7 @@ class Libsoup < Formula
       -I#{gettext.opt_include}
       -I#{glib.opt_include}/glib-2.0
       -I#{glib.opt_lib}/glib-2.0/include
-      -I#{include}/libsoup-2.4
+      -I#{include}/libsoup-3.0
       -D_REENTRANT
       -L#{gettext.opt_lib}
       -L#{glib.opt_lib}
@@ -64,7 +69,7 @@ class Libsoup < Formula
       -lgio-2.0
       -lglib-2.0
       -lgobject-2.0
-      -lsoup-2.4
+      -lsoup-3.0
     ]
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
