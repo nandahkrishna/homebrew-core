ceac3d3f392d88e33f853ba78efcedeaf49c5611
Sean Molenaar <smillerdev@me.com>
2021-09-23 09:28:41 +0200

    libsoup@2 2.74.0 (new formula)

---
+++ Formula/libsoup@2.rb
@@ -0,0 +1,67 @@
+class LibsoupAT2 < Formula
+  desc "HTTP client/server library for GNOME"
+  homepage "https://wiki.gnome.org/Projects/libsoup"
+  url "https://download.gnome.org/sources/libsoup/2.74/libsoup-2.74.0.tar.xz"
+  sha256 "33b1d4e0d639456c675c227877e94a8078d731233e2d57689c11abcef7d3c48e"
+  license "LGPL-2.0-or-later"
+
+  keg_only :versioned_formula
+
+  deprecate! date: "2021-09-18", because: :versioned_formula
+
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "vala" => :build
+  depends_on "glib-networking"
+  depends_on "gnutls"
+  depends_on "libpsl"
+
+  uses_from_macos "krb5"
+  uses_from_macos "libxml2"
+
+  def install
+    mkdir "build" do
+      system "meson", *std_meson_args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
+  end
+
+  test do
+    # if this test start failing, the problem might very well be in glib-networking instead of libsoup
+    (testpath/"test.c").write <<~EOS
+      #include <libsoup/soup.h>
+
+      int main(int argc, char *argv[]) {
+        SoupMessage *msg = soup_message_new("GET", "https://brew.sh");
+        SoupSession *session = soup_session_new();
+        soup_session_send_message(session, msg); // blocks
+        g_assert_true(SOUP_STATUS_IS_SUCCESSFUL(msg->status_code));
+        g_object_unref(msg);
+        g_object_unref(session);
+        return 0;
+      }
+    EOS
+    ENV.libxml2
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    flags = %W[
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/libsoup-2.4
+      -D_REENTRANT
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{lib}
+      -lgio-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lsoup-2.4
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
